3a43c9fd860607cb63f20f3f7a2742821d546f51
lamabot <lamabot@bitbots.org>
2021-05-07 15:30:40 +0200

    webcredits

--- bin/chores.js
+++ bin/chores.js
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
 
-const infile = '../webcredits/webcredits.json'
+const fs = require('fs')
+const infile = __dirname + '/../webcredits/webcredits.json'
 
-const json = require(infile)
+var credits = require(infile)
 
 const points = process.argv[2]
 const secondsSinceEpoch = Math.round(Date.now() / 1000)
 
 if (points) {
   var credit = {amount: points, timestamp: secondsSinceEpoch}
+  credits.push(credit)
 }
 
 console.log(credit)
+
+fs.writeFileSync(infile, JSON.stringify(credits, null, 1))
