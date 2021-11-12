71df239f202601e5e55da014f2263083187d1e78
lamabot <lamabot@bitbots.org>
2021-11-12 22:03:49 +0100

    catch error in ws

--- bin/updater.js
+++ bin/updater.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
 
-
 import { execSync as $ } from 'child_process'
 import WebSocket from 'ws'
 
 var TIME = 300000
 
 var wsdeploy = true
-
 
 function loop() {
   console.log('deploying', process.cwd())
@@ -26,24 +24,27 @@ loop()
 setInterval(loop, TIME)
 
 if (wsdeploy) {
-  const ws = new WebSocket('ws://localhost:4444')
+  try {
+    const ws = new WebSocket('ws://gitmark.me:4444')
 
-  ws.on('open', function open() {
-    console.log('opened')
+    ws.on('open', function open() {
+      console.log('opened')
 
-    var lastCommit = getLastCommit()
-
-    ws.send('sub ' + lastCommit + ':0')
-  })
-
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message)
-    // todo if pub run loop
-    var tuple = message.split(' ')
-    if (tuple[0] === 'pub') {
-      $('git pull origin gh-pages')
       var lastCommit = getLastCommit()
 
-    }
-  })
+      ws.send('sub ' + lastCommit + ':0')
+    })
+
+    ws.on('message', function incoming(message) {
+      console.log('received: %s', message)
+      // todo if pub run loop
+      var tuple = message.split(' ')
+      if (tuple[0] === 'pub') {
+        $('git pull origin gh-pages')
+        lastCommit = getLastCommit()
+      }
+    })
+  } catch (e) {
+    console.error(e)
+  }
 }
