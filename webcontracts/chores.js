548087107fdd4799877f0ee112cc229723b91f5d
lamabot <https://chores.bitbots.org/#me>
2021-12-21 01:08:11 +0100

    add chores webcontract

--- webcontracts/chores.js
+++ webcontracts/chores.js
@@ -1,26 +1,47 @@
 #!/usr/bin/env node
 
+// IMPORTS
 import fs from 'fs'
+import minimist from 'minimist'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-import path from 'path';
-import { fileURLToPath } from 'url';
+// INIT
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const infile = path.join(__dirname, '..', 'webcredits', 'webcredits.json')
+console.log(infile)
 
-const infile = __dirname + '/../webcredits/webcredits.json'
+globalThis.data = {
+  amount: 5,
+  timestamp: Math.round(Date.now() / 1000)
+}
+var argv = minimist(process.argv.slice(2))
+console.log(argv)
 
+data.amount = argv.amount || parseInt(argv._[0]) || data.amount
+data.currency = argv.currency || data.currency
+data.timestamp = argv.timestamp || data.timestamp
+console.log('data', data)
+data.source = argv.source || data.source
+data.destination = argv.destination || data.destination
+data.description = argv.description || data.description
+
+var credit = {}
+if (data.source) credit.source = data.source
+if (data.amount) credit.amount = data.amount
+if (data.currency) credit.currency = data.currency
+if (data.destination) credit.destination = data.destination
+if (data.timestamp) credit.timestamp = data.timestamp
+console.log('credit', credit)
+if (data.description) credit.description = data.description
+
+// MAIN
 var credits = JSON.parse(fs.readFileSync(infile))
-
-
-const points = parseInt(process.argv[2]) || 5
-const secondsSinceEpoch = Math.round(Date.now() / 1000)
-
-if (points) {
-  var credit = { amount: points, timestamp: secondsSinceEpoch }
+if (data.amount) {
   credits.push(credit)
 }
 
-console.log(credit)
-
+// WRITE
 fs.writeFileSync(infile, JSON.stringify(credits, null, 1))
